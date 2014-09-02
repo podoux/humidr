b26d346dcb8ef20078a84d3bcd8ac680448ba31b
ijlyttle <ian.lyttle@schneiderdashelectric.com>
2014-09-02 09:19:09 -0500

    reoxygenize

---
+++ R/psychro.R
@@ -0,0 +1,150 @@
+# reference:
+#  2005 ASHRAE Handbook - Fundamentals (SI)
+#  Chapter 6 - Psychrometrics
+#
+
+#' p_atm_z
+#' 
+#' Pressure as function of elevation/altitude and sea-level pressure
+#' 
+#' Pressure at elevation depends upon the height above sea level and the 
+#' pressure at sea level. In weather reports, generally, sea-level pressure is given.
+#' 
+#' Reference:  2005 ASHRAE Handbook - Fundamentals (SI) ch.6 (3)
+#' 
+#' @param z       numeric: elevation/altitude (m)
+#' @param p_sl    numeric: pressure at sea level, per weather report (Pa), 
+#'                         default 101235 Pa
+#' 
+#' @return        numeric pressure (Pa)
+#' @export
+#' 
+p_atm_z <- function(z, p_sl = 101325) {
+  p_sl * (1 - 2.25577e-5 * z)^5.2559 
+}
+
+#' p_ws_t
+#' 
+#' Saturation pressure of water in air as function of temperature (°C)
+#' 
+#' The saturation pressure of water is the pressure at or above which water
+#' would condense from from the air. It is a function of temperature only.
+#' 
+#' For this function, valid temperature inputs are between -100 and 200 °C.
+#' 
+#' Reference:  2005 ASHRAE Handbook - Fundamentals ch.6 (5), (6)
+#' 
+#' @param t     numeric: temperature, -100 <= t <= 200 (°C)
+#' 
+#' @return      numeric: saturation pressure of water (Pa)
+#' @export
+#' 
+p_ws_t <- function(t){
+ 
+  # validate the input
+  if (any(!(-100 <= t & t <= 200)))
+    stop("Temperature out of range: -100 <= t <= 200")  
+  
+  # define sets of coefficients 
+  cf_cold <- c(-5.6745359e+03,
+               6.3925247e+00,
+               -9.6778430e-03,
+               6.2215701e-07,
+               2.0747825e-09,
+               -9.4840240e-13,
+               4.1635019e+00)
+  
+  cf_warm <- c(-5.8002206e+03,
+               1.3914993e+00,
+               -4.8640239e-02,
+               4.1764768e-05,
+               -1.4452093e-08,
+               0,
+               6.5459673e+00)
+    
+  # determine the saturation vapor pressure,
+  #   use the set of coefficients according to the temperature
+  p_ws <- ifelse(t < 0, .p_ws_c(t, cf_cold), .p_ws_c(t, cf_warm))
+  
+  p_ws  
+}
+
+#' w_pw
+#' 
+#' Humidity ratio as function of vapor pressure, and atmospheric pressure.
+#' 
+#' The humudity ratio is the ratio mass of water vapor to the mass of dry air. 
+#'
+#' @param p_w   numeric: vapor pressure of water (Pa)
+#' @param p_atm numeric: atmospheric pressure (Pa), default 101235 Pa
+#' 
+#' Reference:  2005 ASHRAE Handbook - Fundamentals ch.6 (3)
+#' 
+#' @return numeric: humidity ratio (1)
+#' @export
+#' 
+w_pw <- function(p_w, p_atm = 101325) 0.62198 * p_w / (p_atm - p_w)
+
+#' w_t_rh
+#' 
+#' Humidity ratio as function of (dry-bulb) temperature and relative humidity
+#' 
+#' The humudity ratio is the ratio mass of water vapor to the mass of dry air. 
+#' 
+#' @param t       numeric: temperature (°C)
+#' @param p_atm   numeric: atmospheric pressure (Pa), default 101235 Pa
+#' 
+#' @return numeric: humidity ratio (1)
+#' @export
+#' 
+w_t_rh <- function(t, rh, p_atm = 101325){
+  
+  # saturation vapor pressure is a function of temperature
+  p_ws <- p_ws_t(t)
+  
+  # find the vapor pressure using the realtive humidity
+  #   relative humidity is the ratio of vapor pressure to the 
+  #   saturation vapor pressure, at a given temperature
+  p_w <- rh*p_ws
+  
+  # humidity ratio depends on the vapor pressure, and the atmospheric pressure
+  w <- w_pw(p_w, p_atm)
+  
+  w
+}
+
+
+#' h_t_w
+#' 
+#' Enthalpy of moist air as function of temperature and humidity ratio
+#' 
+#' The reference conditions, where enthalpy is 0 kJ/kg, 
+#' are zero-moisture air at 0 °C.
+#' 
+#' @param t   numeric: temperaure (°C)
+#' @param w   numeric: humidity ratio (1)
+#' 
+#' @return    numeric: enthalpy of dry air (kJ/kg)
+#' @export
+#' 
+h_t_w <- function(t, w) 1.006*t + w*(2501 + 1.86*t) 
+
+#' h_t_rh
+#' 
+#' The reference conditions, where enthalpy is 0 kJ/kg, 
+#' are zero-moisture air at 0 °C.
+#' 
+#' @param t     numeric: temperaure (°C)
+#' @param rh    numeric: relative humidity (1)
+#' @param p_atm numeric: atmospheric pressure (Pa)
+#' 
+#' @return      numeric: enthalpy of dry air (kJ/kg)
+#' @export
+#' 
+h_t_rh <- function(t, rh, p_atm=101325){
+  w <- w_t_rh(t, rh, p_atm)
+  h <- h_t_w(t, w)
+  
+  return(h)
+} 
+
---
+++ R/psychro_internal.R
@@ -0,0 +1,98 @@
+# reference:
+#  2005 ASHRAE Handbook - Fundamentals (SI)
+#  Chapter 6 - Psychrometrics
+#
+ 
+# .p_ws_c
+# 
+# Internal function used to calculate the saturation pressure
+#
+# @param t   numeric: temperature (°C)
+# @param cf  numeric: set of coefficients from ASHRAE, 
+#                     depending on temperature 
+#
+# @return    numeric: saturation pressure (Pa)
+#
+.p_ws_c <- function(t, cf){
+  
+  # convert to Kelvin
+  t <- t + 273.15
+  
+  # determine the logarithm of the saturation pressure
+  ln_p_ws <- cf[1]/t +
+    ((((cf[6]*t) + cf[5])*t + cf[4])*t + cf[3])*t + cf[2] +
+    cf[7]*log(t)
+ 
+  # determine the saturation pressure
+  p_ws <- exp(ln_p_ws)
+  
+}
+  
+
+# .h_evap 
+# 
+# Internal function
+#
+# Enthalpy of evaporation of water (kJ/kg)
+# 
+# @param t numeric temperature (°C)
+# 
+# @return numeric enthalpy of evaporation of water (kJ/kg)   
+# 
+.h_evap <- function(t) 4.186*t # kJ/kg
+
+
+# .h_sub
+#
+# Internal function
+# 
+# enthalpy of sublimation of water (kJ/kg)
+# 
+# @param t numeric temperature (°C)
+# 
+# @return numeric enthalpy of sublimation of water (kJ/kg)   
+# 
+.h_sub <- function(t) -333.4 + 2.1*t # kJ/kg
+
+# .error_wetbulb
+# 
+# given guess for saturation temperature, 
+# plus temperature, relative humidity, pressure,
+# determine error in energy balance
+# 
+# @param t_sat numeric saturation temperaure (degC) (guess for wet-bulb)
+# @param t numeric temperature (degC)
+# @param rh numeric relative humidity (1)
+# @param p_atm numeric atmospheric pressure (Pa)
+# 
+.error_wetbulb <- function(t_sat, t, rh, p_atm=101325){
+  
+  h <- h_t_rh(t, rh, p_atm)
+  dw <- w_t_rh(t_sat, 1, p_atm) - w_t_rh(t, rh, p_atm)
+  h_change <- ifelse(t_sat < 0, .h_sub(t_sat), .h_evap(t_sat))
+  h_sat <- h_t_rh(t_sat, 1, p_atm)
+  
+  resid <- h + dw*h_change - h_sat 
+  
+  return(resid)
+}
+
+# .t_wb_single
+# 
+# for a single set of conditions, determine wet-bulb temperature
+# 
+# @param t numeric temperature (degC)
+# @param rh numeric relative humidity (1)
+# @param p_atm numeric atmospheric pressure (Pa)
+# 
+# @return numeric wet-bulb temperature (degC)
+# 
+.t_wb_single <- function(t, rh, p_atm = 101325){
+  
+  t_wb <- uniroot(.error_wetbulb, t=t, rh=rh, lower = -100, upper = 65)$root
+  
+  return(t_wb)
+  
+}
+
+
